c4354ee0ef93dc817bca931654fcb5955822e591
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-15 20:53:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function writeCards ( namesArray, event ) {
-  let thankYouCards = [] 
-  for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
   return thankYouCards
-}
-
-function countDown ( startingNumber) {
-  while (startingNumber > 0) {
-    console.log(startNumber); 
-    startingNumber -= 1;
-  }
-  console.log (startingNumber);
 }
