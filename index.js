9fbc94826539efc7f8b31597f36a17a3e03fa446
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-15 20:50:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log('Wrapped ${gifts[i]} and added a bow!');
-    
-  }
-  
-  return gifts;
-}
-
+function writeCards 
